7afdc4b17c6d9510d299470b83c0e576d805d21b
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-05-13 00:15:20 +0530

    fixed typo

--- update-screenshot.js
+++ update-screenshot.js
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer';
  (async () => {
-    const browser = await puppeteer.lunch({headless:true});
+    const browser = await puppeteer.launch({headless:true});
     const page = await browser.newPage();
     await page.goto('https://PasiduRanasinghe.github.io/Screenshot-Pipeline');
     await page.screenshot({path:'screenshot.png', fullPage:true});
