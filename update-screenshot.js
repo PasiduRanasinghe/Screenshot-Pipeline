9b47df8233f3d27cb84d3a6514f0dc96008f87e1
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-05-13 00:27:47 +0530

    fixed a bug added wait untill network idle

--- update-screenshot.js
+++ update-screenshot.js
@@ -1,8 +1,11 @@
-import puppeteer from 'puppeteer';
- (async () => {
-    const browser = await puppeteer.launch({headless:true});
-    const page = await browser.newPage();
-    await page.goto('https://PasiduRanasinghe.github.io/Screenshot-Pipeline');
-    await page.screenshot({path:'screenshot.png', fullPage:true});
-    await browser.close();
- })
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto("https://PasiduRanasinghe.github.io/Screenshot-Pipeline", {
+    waitUntil: "networkidle2",
+  });
+  await page.screenshot({ path: "screenshot.png", fullPage: true });
+  await browser.close();
+})();
