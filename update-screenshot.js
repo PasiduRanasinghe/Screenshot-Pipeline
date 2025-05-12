4e112b3d15a42e724b1c30f074b71570b5ff2cf0
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-05-13 01:15:00 +0530

    change puppeteer to puppeteer-core

--- update-screenshot.js
+++ update-screenshot.js
@@ -1,7 +1,12 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"], });
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath:
+      process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.goto("https://PasiduRanasinghe.github.io/Screenshot-Pipeline", {
     waitUntil: "networkidle2",
